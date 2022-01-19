fe24097a2008148bdc3fad25d9dac61c7924b4b8
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2022-01-19 22:21:43 +0100

    update

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -1,5 +1,6 @@
-{}:
+{ ... }:
 {
   # Enable Wayland backend
-  nixpkgs.config.chromium.commandLineArgs = "--ozone-platform-hint=auto";
+  # Enable hardware video decoding
+  nixpkgs.config.chromium.commandLineArgs = "--ozone-platform-hint=auto --enable-features=VaapiVideoDecoder";
 }
